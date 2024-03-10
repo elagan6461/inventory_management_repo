ae87f8a73c626bbb6c2e98e54b23a9dd2f7bc04e
Elaina Gantan <138708091+elagan6461@users.noreply.github.com>
2024-03-10 14:43:53 -0400

    Delete routes directory

--- routes/inventory.routes.js
+++
@@ -1,9 +0,0 @@
-const InventoryController = require('../controllers/inventory.controller')
-
-module.exports = (app) => {
-    app.get('/api/allItems', InventoryController.findAllItems)
-    app.get('/api/findOneItem/:id', InventoryController.findOneItem)
-    app.post('/api/createItem', InventoryController.createItem)
-    app.put('/api/updateItem/:id', InventoryController.updateItem)
-    app.delete('/api/deleteItem/:id', InventoryController.deleteItem)
-}
