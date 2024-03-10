d88bf8e27343203ffb30e8933bee509a31f3d39c
Elaina Gantan <138708091+elagan6461@users.noreply.github.com>
2024-03-10 14:43:28 -0400

    Delete config directory

--- config/mongoose.config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
- 
-mongoose.connect('mongodb://127.0.0.1:27017/inventoryDB')
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
-
