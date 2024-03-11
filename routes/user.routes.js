6ae739c273128249320370fc9b59219c3daf78f3
Elaina Gantan <138708091+elagan6461@users.noreply.github.com>
2024-03-11 19:52:48 -0400

    Delete routes directory

--- routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const UserController = require('../controllers/user.controller')
-
-module.exports = (app) => {
-    app.post('/api/registerUser', UserController.registerUser)
-    app.post('/api/loginUser', UserController.loginUser)
-    app.post('/api/logoutUser', UserController.logoutUser)
-}
