dec5ffbc5a0ff2f217213c2b0801d0a01c4223b1
Elaina Gantan <138708091+elagan6461@users.noreply.github.com>
2024-03-11 19:56:33 -0400

    Update server for login and registration

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-
-app.use(cors())
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const cookieParser = require('cookie-parser')
 app.use(express.json(), express.urlencoded({ extended:true }))
+app.use(cors({credentials:true, origin:'http://localhost:5173'}))
+app.use(cookieParser())
+require('./config/mongoose.config')
+const userRoutes = require('./routes/user.routes')
+userRoutes(app)
 
 require('./config/mongoose.config')
 
 require('./routes/inventory.routes')(app)
 
-app.listen(8000, () => console.log('The server is running on Port 8000'))
+app.listen(8000, () => console.log('The server is running on Port 8000'));
