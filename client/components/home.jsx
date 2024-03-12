c81d11288a4fff48fdcb142e981719e1851969ac
Elaina Gantan <138708091+elagan6461@users.noreply.github.com>
2024-03-11 20:10:51 -0400

    Add logout button and axios post call for logging out

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 const Home = (props) => {
+    const navigate = useNavigate()
+    const logout = () => {
+        axios.post('http://localhost:8000/api/logoutUser', {}, {withCredentials:true})
+            .then(navigate('/'))
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    
     return (
         <>
         <h1>Inventory Tracker</h1>
@@ -9,6 +19,7 @@ const Home = (props) => {
             <Link to={'/inventoryList'} className='look_like_a_button'>Inventory List</Link>
             <Link to={'/searchInventory'}className='look_like_a_button'>Search Inventory</Link>
             <Link to={'/addItem'}className='look_like_a_button'>Add Item</Link>
+            <button onClick={logout} className='look_like_a_button'>Logout</button>
         </nav>
         
         </>
