bf0f7dfe57f49de455a53618785ed78369cddadf
Elaina Gantan <138708091+elagan6461@users.noreply.github.com>
2024-03-11 21:02:18 -0400

    Update database name

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
  
-mongoose.connect('mongodb://127.0.0.1:27017/inventoryDB')
+mongoose.connect('mongodb://127.0.0.1:27017/inventoryDB2')
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
 
